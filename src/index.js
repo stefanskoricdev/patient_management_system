667efc6b842bac263334c41cc6299d21c5127200
Stefan Skoric <stephanskoric@gmail.com>
2022-01-23 21:16:27 +0100

    Add ScrollToTop component to make top scroll smooth after switching routes

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { AppProvider } from "./store/AppProvider";
 import { AuthProvider } from "./store/AuthProvider";
 import App from "./App";
 import "./index.scss";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <AppProvider>
-          <App />
+          <ScrollToTop>
+            <App />
+          </ScrollToTop>
         </AppProvider>
       </AuthProvider>
     </BrowserRouter>
