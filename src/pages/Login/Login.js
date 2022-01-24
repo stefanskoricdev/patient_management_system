5f429074fb636c352a45cb49ecb832b7b33da136
Stefan Skoric <stephanskoric@gmail.com>
2022-01-24 10:12:08 +0100

    Update firebaserc to client app config. Remove welcome message in LoginPage

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,21 +1,14 @@
 import styles from "./Login.module.scss";
-import { Fragment, useContext, useState } from "react";
+import { Fragment, useContext } from "react";
 import logo from "../../assets/img/logo.png";
 import wave from "../../assets/img/wave.png";
 import AppContext from "../../store/AppProvider";
 import Loader from "../../components/UI/Loader/Loader";
 import LoginForm from "./LoginForm/LoginForm";
-import LoginInfo from "./LoginInfo/LoginInfo";
-import Backdrop from "../../components/UI/Backdrop/Backdrop";
 
 const Login = () => {
   const appCtx = useContext(AppContext);
   const { setIsLoading, isLoading } = appCtx;
-  const [showInfo, setShowInfo] = useState(true);
-
-  const handleClick = () => {
-    setShowInfo(false);
-  };
 
   return (
     <Fragment>
@@ -27,11 +20,6 @@ const Login = () => {
         </section>
         <LoginForm setLoading={setIsLoading} />
       </section>
-      {!isLoading && showInfo && (
-        <Backdrop>
-          <LoginInfo handleClick={handleClick} />
-        </Backdrop>
-      )}
     </Fragment>
   );
 };
