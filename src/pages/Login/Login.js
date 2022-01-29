15bdb61129afe318114caeb79110fb15f9d430f1
Stefan Skoric <stephanskoric@gmail.com>
2022-01-29 18:34:32 +0100

    Added Login Help window

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,22 @@
 import styles from "./Login.module.scss";
-import { Fragment, useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import logo from "../../assets/img/logo.png";
 import wave from "../../assets/img/wave.png";
 import AppContext from "../../store/AppProvider";
 import Loader from "../../components/UI/Loader/Loader";
 import LoginForm from "./LoginForm/LoginForm";
+import LoginInfo from "./LoginInfo/LoginInfo";
+import Backdrop from "../../components/UI/Backdrop/Backdrop";
 
 const Login = () => {
   const appCtx = useContext(AppContext);
   const { setIsLoading, isLoading } = appCtx;
 
+  const [showInfo, setShowInfo] = useState(true);
+
+  const handleClick = () => {
+    setShowInfo(false);
+  };
   return (
     <Fragment>
       {isLoading && <Loader />}
@@ -19,6 +26,11 @@ const Login = () => {
           <img className={styles.LoginImg} src={logo} alt="login" />
         </section>
         <LoginForm setLoading={setIsLoading} />
+        {!isLoading && showInfo && (
+          <Backdrop>
+            <LoginInfo handleClick={handleClick} />
+          </Backdrop>
+        )}
       </section>
     </Fragment>
   );
